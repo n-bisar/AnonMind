228f6b6e5d766a5134e358ab9238eae944a778ef
Bisar <bisar.nawaz2004@gmail.com>
2026-09-06 23:56:44 +0530

    Updating features

--- frontend/src/context/ThemeContext.jsx
+++ frontend/src/context/ThemeContext.jsx
@@ -3,7 +3,9 @@ import { createContext, useState, useEffect } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(
+  () => localStorage.getItem("theme") || "light"
+);
 
   useEffect(() => {
     document.documentElement.classList.toggle(
