6f616e9c0fff942d6072bbe210b51daf7aba011b
Bisar <bisar.nawaz2004@gmail.com>
2026-09-07 22:58:09 +0530

    Corrected the Navbar Mobile version

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext,useState } from "react";
 import ThemeContext from "../context/ThemeContext";
 import Button from "./Button";
 
 
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const { theme,setTheme  } = useContext(ThemeContext);
   return (
     <nav className="w-full border-b border-border bg-surface">
@@ -11,15 +12,24 @@ function Navbar() {
         <div className="text-2xl font-bold text-text">
           AnonMind
         </div>
-
-        <div className="flex items-center gap-3">
-
-            <button
+      <div className="flex items-center gap-3">
+        <button
                 onClick={() => setTheme(theme === "light" ? "dark" : "light")}
                 className="rounded-lg border border-border bg-surface px-3 py-2 text-text transition-colors duration-200 hover:bg-background"
             >
                 {theme === "light" ? "🌙" : "☀️"}
             </button>
+
+        <button
+  onClick={() => setIsMenuOpen(!isMenuOpen)}
+  className="rounded-lg border border-border bg-surface px-3 py-2 text-text md:hidden"
+>
+  {isMenuOpen ? "✕" : "☰"}
+</button>
+
+        <div className="hidden items-center gap-3 md:flex">
+
+            
 
           <Button variant="secondary">
             Login
@@ -28,8 +38,22 @@ function Navbar() {
           <Button>
             Get Started
           </Button>
+          </div>
         </div>
       </div>
+      {isMenuOpen && (
+  <div className="border-t border-border bg-surface px-6 py-4 md:hidden">
+    <div className="flex flex-col gap-3">
+      <Button variant="secondary">
+        Login
+      </Button>
+
+      <Button>
+        Get Started
+      </Button>
+    </div>
+  </div>
+)}
     </nav>
   );
 }
